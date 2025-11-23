770221b81b4a86de1590a97c7da1d22dd32e068a
Mick <wang.mick12@gmail.com>
2025-11-22 23:19:30 -0500

    add backend for user login

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -27,17 +27,23 @@ router.post("/register", async (req, res) => {
         .json({ message: "Username must be at least 3 characters long" });
     }
 
-    // Check if user already exists
-    const existing = await User.findOne({ $or: [{ email }, { username }] });
-    if (existing)
-      return res.status(400).json({ message: "User already exists" });
+    // check if user already exists
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
 
     const profileImage = `https://api.dicebear.com/9.x/identicon/svg?seed=${username}`;
     const newUser = new User({ username, email, password, profileImage });
 
     await newUser.save();
 
-    const token = generateToken(newUser);
+    const token = generateToken(newUser._id);
 
     res
       .status(201)
@@ -55,9 +61,40 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
-router.post("/login", (req, res) => {
-  // Login logic here
-  res.send("login");
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user._id);
+    res
+      .status(200)
+      .json({
+        token,
+        user: {
+          _id: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          profileImage: newUser.profileImage,
+        },
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 export default router;
